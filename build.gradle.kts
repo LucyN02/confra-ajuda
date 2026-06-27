295fadb2bd52a5a40fecaad31f0500e9f55b184b
Lucy Neves <luciananbrito22@gmail.com>
2026-06-26 23:26:49 -0300

    fix version java

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ application {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
-        jvmTarget = "21"
+        jvmTarget = "17"
     }
 }
 
